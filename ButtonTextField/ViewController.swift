dd9afa9a2a233bd2e23dc52682fe188224e1e8e8
COE-01 <coe-01@coe-01s-imac.local>
2019-08-22 15:31:17 +0530

    Program to test button and label

--- ButtonTextField/ViewController.swift
+++ ButtonTextField/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Outlets
+    
+    @IBOutlet weak var txtEmailAdress: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var txtConfirmPassword: UITextField!
+    @IBOutlet weak var lblMessage: UILabel!
+    @IBOutlet weak var lblPrintLoginCredentials: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +29,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func btnSignUpTouchUpInside(_ sender: Any) {
+        let emailid = txtEmailAdress.text
+        let password = txtPassword.text
+        let confirm = txtConfirmPassword.text
+        
+        if password != confirm {
+            lblMessage.text = "Confirmation password does no match!"
+            lblPrintLoginCredentials.text = ""
+        }
+        else{
+            lblMessage.text = "Login succesful!"
+            lblPrintLoginCredentials.text = "Email:\(emailid!) Password:\(password!)"
+        }
+        
+        
+        
+    }
+    @IBAction func btnPasswordVisibilityValueChange(_ sender: Any) {
+        txtPassword.isSecureTextEntry = !(txtPassword.isSecureTextEntry)
+        txtConfirmPassword.isSecureTextEntry = !(txtConfirmPassword.isSecureTextEntry)
+    }
+    
+    
+    
 }
 
